f4d2ce91e800e62f5d3177f2bb344b5516af2e81
Wolfyxon <wolfyxon@gmail.com>
2026-02-01 22:15:13 +0100

    Removed config path log

--- src/main.c
+++ src/main.c
@@ -7,8 +7,6 @@ int main(int argc, char** argv) {
     printf("https://github.com/Wolfyxon/morgan\n\n");
     printf("Starting...\n");
     
-    printf("%s \n", config_get_dir());
-
     input_init();    
     midi_init();
     
