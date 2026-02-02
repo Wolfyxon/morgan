169d201973349abc086d1b8de7d561b9a60ad842
Wolfyxon <wolfyxon@gmail.com>
2026-02-02 13:03:34 +0100

    Return value must be reversed

--- src/windows/fs.c
+++ src/windows/fs.c
@@ -1,9 +1,9 @@
 #include <windows.h>
 
 int mkdir(char* path, unsigned int mode) {
-    return CreateDirectory(path, NULL);
+    return !CreateDirectory(path, NULL);
 }
 
 int rmdir(char* path) {
-    return RemoveDirectory(path);
+    return !RemoveDirectory(path);
 }
