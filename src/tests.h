fa669c871b5ae967afbc6b9f5af5415be30eb8cd
Wolfyxon <wolfyxon@gmail.com>
2026-02-03 15:24:36 +0100

    Removed test passed message

--- src/tests.h
+++ src/tests.h
@@ -16,7 +16,6 @@ void run_tests();
 #define run_test(function) do { \
     printf("-- Running '%s' -- \n", #function); \
     function(); \
-    printf("> Test passed \n"); \
 } while(0);
 
 void test_test();
