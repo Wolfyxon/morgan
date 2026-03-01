b3b5719a25cbf5b605c9ef749c00614f0e603cf4
Wolfyxon <wolfyxon@gmail.com>
2026-03-01 16:03:09 +0100

    Fixed invalid file reading

--- src/fs.c
+++ src/fs.c
@@ -10,7 +10,7 @@ char* freadstr(char* path) {
     char* file_buf = malloc(0);
     size_t file_len = 0;
 
-    char read_buf[64];
+    char read_buf[64] = {0};
     size_t read_len = 0;
 
     if(file_buf == NULL) {
@@ -20,9 +20,8 @@ char* freadstr(char* path) {
         return NULL;
     }
 
-    while((read_len = fread(read_buf, sizeof(char), sizeof(read_buf), file)) != 0) {
-        file_len += read_len;
-        file_buf = realloc(file_buf, file_len);
+    while((read_len = fread(read_buf, 1, sizeof(read_buf), file)) > 0) {
+        file_buf = realloc(file_buf, file_len + 1);
 
         if(!file_buf) {
             fprintf(stderr, "error: Failed to extend file buffer. \n");
@@ -33,11 +32,16 @@ char* freadstr(char* path) {
             return NULL;
         }
 
-        strncat(file_buf, read_buf, read_len);
+        memcpy(file_buf + file_len, read_buf, read_len);
+        file_len += read_len;
     }
 
     fclose(file);
 
+    if(file_len != 0) {
+        file_buf[file_len - 1] = '\0';
+    }
+    
     return file_buf;
 }
 
