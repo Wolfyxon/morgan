5569aa466c9dcc479b276aaf27043800da539ab3
Wolfyxon <wolfyxon@gmail.com>
2026-03-20 16:23:06 +0100

    Checking if XDG_SESSION_TYPE exists, also fixes segfault

--- src/linux/linux.c
+++ src/linux/linux.c
@@ -3,6 +3,11 @@
 SessionType linux_get_session_type() {
     char* session = getenv("XDG_SESSION_TYPE");
 
+    if(session == NULL) {
+        fprintf(stderr, "error: XDG_SESSION_TYPE not detected. Morgan must be running under Xorg\n");
+        exit(1);
+    }
+    
     if(strcmp(session, "x11") == 0) {
         return SESSION_X11;
     }
